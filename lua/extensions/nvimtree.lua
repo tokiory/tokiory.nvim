31bb96505a8c6a80a8ee9282b352e52d637cf3a1
tokiory <tokiory.personal@gmail.com>
2023-07-07 21:45:49 +0300

    feat(nvim-tree): add nvim-tree plugin

---
+++ lua/extensions/nvimtree.lua
@@ -0,0 +1,24 @@
+nvim_tree = require('nvim-tree')
+
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+nvim_tree.setup({
+  view = {
+    width = 35
+  },
+  on_attach = my_on_attach
+})
